34d3f2336f8dcf404ebb882bfe4c8c2122a001e5
Nithin <vooturinithinkumar@gmail.com>
2026-01-12 18:07:56 +0530

    add products

---
+++ src/data/products.js
@@ -0,0 +1,69 @@
+const products = [
+
+    {
+        id: 1,
+        name: "iPhone 14",
+        brand: "Apple",
+        price: 70000,
+        rating: 4.5,
+        inStock: true
+    },
+    {
+        id: 2,
+        name: "Galaxy S23",
+        brand: "Samsung",
+        price: 65000,
+        rating: 4.4,
+        inStock: false
+    },
+    {
+        id: 3,
+        name: "Pixel 7",
+        brand: "Google",
+        price: 60000,
+        rating: 4.3,
+        inStock: true
+    },
+
+    {
+        id: 4,
+        name: "OnePlus 11",
+        brand: "OnePlus",
+        price: 58000,
+        rating: 4.2,
+        inStock: true
+    },
+    {
+        id: 5,
+        name: "Xiaomi 13 Pro",
+        brand: "Xiaomi",
+        price: 55000,
+        rating: 4.1,
+        inStock: true
+    },
+    {
+        id: 6,
+        name: "Nothing Phone (2)",
+        brand: "Nothing",
+        price: 45000,
+        rating: 4.0,
+        inStock: false
+    },
+    {
+        id: 7,
+        name: "iQOO 11",
+        brand: "iQOO",
+        price: 50000,
+        rating: 4.2,
+        inStock: true
+    },
+    {
+        id: 8,
+        name: "Realme GT 3",
+        brand: "Realme",
+        price: 42000,
+        rating: 3.9,
+        inStock: true
+    }
+]
+export default products;
